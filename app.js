77751b97c220ce24d6152e65f4a76856e9554e78
Edder E <34556439+3dd3r@users.noreply.github.com>
2020-10-01 18:42:15 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+// Declaring Variables
+
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDom = document.querySelector(".products-center");
+
+// Cart Items
+let cart = [];
+
+// getting Products
+class Products {
+    async getProducts() {
+        try {
+            const result = await fetch("products.json");
+            let data = await result.json();
+            return data
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+
+// display Products
+class UI { }
+
+// local Storage
+class Storage { }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const ui = new UI();
+    const products = new Products();
+
+    // get all products
+    products.getProducts().then((value) => {
+        console.log(value);
+    })
+});
