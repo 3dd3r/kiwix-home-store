1c861f99a9e75828275f52e849b5d061d910f5d2
Edder E <34556439+3dd3r@users.noreply.github.com>
2020-10-01 23:12:36 +0100

    sec commit

--- app.js
+++ app.js
@@ -19,7 +19,13 @@ class Products {
         try {
             const result = await fetch("products.json");
             let data = await result.json();
-            return data
+            let products = data.items;
+            products = products.map(item => {
+                const {title, price} = item.fields;
+                const {id} = item.sys;
+                const {image} = item.fields.image.file.url;
+                return {id, title, price, image}
+            })
         } catch (error) {
             console.log(error);
         }
